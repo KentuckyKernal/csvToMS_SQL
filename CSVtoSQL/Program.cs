a5819a8abf4f06adf9c30a896ba705103da09699
-- The Kentucky Kernel <kieranrafter01@gmail.com>
2022-03-08 21:23:33 +0000

    Update Program.cs

--- CSVtoSQL/Program.cs
+++ CSVtoSQL/Program.cs
@@ -8,24 +8,11 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            
             GlobalVars glv = new GlobalVars();
-
-            //Console.WriteLine("Enter Directory Where Files are stored: ");
-            //glv.Path = @"" + Console.ReadLine() + "";
-
-            //Console.WriteLine("\r\n");
-
-            //Console.WriteLine("Enter connection string: ");
-            //glv.Conn = @"" + Console.ReadLine() + "";
-            //Console.WriteLine("\r\n");
-
-            //Console.WriteLine("Please Type Destination Database: ");
-            //glv.Dtb = @"" + Console.ReadLine() + "";
 
             DirSelect start = new DirSelect();
             start.Output(glv.Path, glv.Conn, glv.Dtb);
-
-
 
         }
     }
